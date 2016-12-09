bba152059617cd11b5e0e70a04e53c3c2ad95d73
SKYUSER-4D008AE\win10 <Notkak1994@gmail.com>
2016-12-09 17:45:15 +0700

    kuy

--- application/views/user_backend/show_prop.php
+++ application/views/user_backend/show_prop.php
@@ -26,56 +26,15 @@ font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-d
 					</li>
 
 
-                    <li>
-                        <a  href="index.html"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
-                    </li>
-                   <li>
-                        <a  href="ui.html"><i class="fa fa-desktop fa-3x"></i> UI Elements</a>
-                    </li>
-                    <li>
-                        <a  href="tab-panel.html"><i class="fa fa-qrcode fa-3x"></i> Tabs & Panels</a>
-                    </li>
-						   <li  >
-                        <a  href="chart.html"><i class="fa fa-bar-chart-o fa-3x"></i> Morris Charts</a>
-                    </li>
-                      <li  >
-                        <a class="active-menu"  href="table.html"><i class="fa fa-table fa-3x"></i> Table Examples</a>
-                    </li>
-                    <li  >
-                        <a  href="form.html"><i class="fa fa-edit fa-3x"></i> Forms </a>
-                    </li>
-
-
-                    <li>
-                        <a href="#"><i class="fa fa-sitemap fa-3x"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Second Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Second Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                                <ul class="nav nav-third-level">
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level Link</a>
-                                    </li>
-
-                                </ul>
-
-                            </li>
-                        </ul>
-                      </li>
-                  <li  >
-                        <a   href="blank.html"><i class="fa fa-square-o fa-3x"></i> Blank Page</a>
-                    </li>
+          <li>
+              <a  href="<?php echo base_url('user_backend/dashboard'); ?>"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
+          </li>
+            <li>
+              <a  href="<?php echo base_url('user_backend/addprop'); ?>"><i class="fa fa-desktop fa-3x"></i> Create Property</a>
+          </li>
+          <li>
+              <a  href="<?php echo base_url('user_backend/editprofile'); ?>"><i class="fa fa-qrcode fa-3x"></i> Edit Profile</a>
+          </li>
                 </ul>
 
             </div>
